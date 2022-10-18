c2baf2de6add152db036015478ef5d12db69365e
DHintz137 <102270046+DHintz137@users.noreply.github.com>
2022-10-18 11:49:10 -0600

    Initial repo set up

---
+++ datasummary2/R/square_val.R
@@ -0,0 +1,19 @@
+#' square a value
+#'
+#' @param x a numeric value to be squared
+#'
+#' @return a number
+#' @export
+#'
+#' @examples
+#' square_val(2)
+square_val <- function(x) {
+  x^2
+}
+# devtools::load_all()
+# cmd + shift + P
+
+# devtools::document()
+# devtools::install()
+library(datasummary2)
+?square_val
---
+++ datasummary2/R/test-square_val.R
@@ -0,0 +1,3 @@
+testthat::test_that("actally squares", {
+  expect_equal(square_val(2),4)
+})
